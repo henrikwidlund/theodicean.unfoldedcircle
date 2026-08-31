71546d870bdca7f4d1155ca33f807aa92f25da12
Henrik Widlund <4659350+henrikwidlund@users.noreply.github.com>
2026-08-31 20:37:41 +0200

    Make GridItemSize deserializable with default values

--- src/UnfoldedCircle.Models/Events/GridItemSize.cs
+++ src/UnfoldedCircle.Models/Events/GridItemSize.cs
@@ -3,11 +3,6 @@ namespace UnfoldedCircle.Models.Events;
 /// <summary>
 /// Item size in the button grid. Default size if not specified: 1 x 1
 /// </summary>
-public record GridItemSize
-{
-    [JsonPropertyName("width")]
-    public ushort Width { get; init; } = 1;
-
-    [JsonPropertyName("height")]
-    public ushort Height { get; init; } = 1;
-}
+public record GridItemSize(
+    [property: JsonPropertyName("width")] ushort Width = 1,
+    [property: JsonPropertyName("height")] ushort Height = 1);
